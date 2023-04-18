56ede338c6f05c2b9e4536a5dc642e164e48e486
javierperezramirez <115183114+javierperezramirez@users.noreply.github.com>
2023-04-18 20:29:15 +0200

    Update javascript6.js

--- javascript6.js
+++ javascript6.js
@@ -6,10 +6,27 @@ let cuadro = prompt("Diga en qué cuadro quiere saludar (1 a 5)","");
 cuadro = parseInt(cuadro);
 // 'cuadro' será una variable numérica entre 1 y 5
 
+var color = prompt("Diga de que color quiere la piramide (a=Blue, b=Red, c=Green)","");
+
 if ((cuadro<1)||(cuadro>5)||isNaN(cuadro)) {
     alert("El numero introducido NO es valido!!!")
 } else {
-    var selector = "c"+cuadro;
-    var estecuadro = document.getElementById(selector);
-    estecuadro.innerHTML="HOLA";
+    if (color=='a') {
+        let selector = "c"+cuadro;
+        let estecuadro = document.getElementById(selector);
+        estecuadro.innerHTML="--o--"+'<br>'+"-ooo-"+'<br>'+"ooooo";
+        estecuadro.style.color="blue";
+    } else if (color=='b') {
+        let selector = "c"+cuadro;
+        let estecuadro = document.getElementById(selector);
+        estecuadro.innerHTML="--o--"+'<br>'+"-ooo-"+'<br>'+"ooooo";
+        estecuadro.style.color="red";
+    } else if (color=='c') {
+        let selector = "c"+cuadro;
+        let estecuadro = document.getElementById(selector);
+        estecuadro.innerHTML="--o--"+'<br>'+"-ooo-"+'<br>'+"ooooo";
+        estecuadro.style.color="green";
+    } else {
+        alert("El color introducido no es valido")
+    }
 }
