594f16bad1ad147d6b9c0a7e90a2f6da73cfb0e4
javierperezramirez <115183114+javierperezramirez@users.noreply.github.com>
2023-04-25 19:46:56 +0200

    Update javascript7.js

--- javascript7.js
+++ javascript7.js
@@ -1,7 +1,10 @@
 'use strict'
 // 'use strict' nos ayuda a no dejar pasar ningun error
 
-// creamos una funcion que pinte el cuadro con un hover
+// creamos una variable para contar las veces que pinta
+var contador=0;
+
+// creamos una funcion que pinte el cuadro con un hover (activo)
 function hoverino(identificador) {
     let id="c"+identificador;
     let cuadro=document.getElementById(id);
@@ -10,12 +13,18 @@ function hoverino(identificador) {
 
 // creamos una funcion generica para dibujar el simbolo
 function pintar(identificador) {
-    let id="c"+identificador;
-    let cuadro=document.getElementById(id);
-    cuadro.style.color='black';
-    cuadro.innerHTML="*";
+    
+    // ponemos un if para limitar a 3 los dibujos
+    if (contador<3) {
+        let id="c"+identificador;
+        let cuadro=document.getElementById(id);
+        cuadro.style.color='black';
+        cuadro.innerHTML="*";
+        contador+=1;
+    }
 }
 
+// creamos una funcion que pinta el cuadro con un hover (inactivo)
 function hoverinoff(identificador) {
     let id="c"+identificador;
     let cuadro=document.getElementById(id);
