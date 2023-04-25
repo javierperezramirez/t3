6f5adec15ff657d524b4877131a7708537588247
javierperezramirez <115183114+javierperezramirez@users.noreply.github.com>
2023-04-25 20:46:49 +0200

    Update javascript7.js

--- javascript7.js
+++ javascript7.js
@@ -5,7 +5,8 @@
 var contador=0;
 
 // creamos un array para el tablero
-var tablero= [3,3];
+var tablero = [['0','0','0'],['0','0','0'],['0','0','0']];
+
 
 // creamos una funcion que pinte el cuadro con un hover (activo)
 function hoverino(identificador) {
@@ -24,8 +25,9 @@ function pintar(identificador) {
         cuadro.style.color='black';
         cuadro.innerHTML="*";
         contador+=1;
+        tablero[identificador]='1';
     } else {
-        alert("El juego ha terminado, reinicia la partida!!!");
+        compruebaMatriz();
     }
 }
 
@@ -34,4 +36,14 @@ function hoverinoff(identificador) {
     let id="c"+identificador;
     let cuadro=document.getElementById(id);
     cuadro.style.backgroundColor='rgb(0, 218, 218)';
+}
+
+function compruebaMatriz() {
+    for (var i=0;i<3;i++) {
+        for (var j=0;j<3;j++) {
+            if ((tablero[i,j]==1)&&(tablero[i,j+1])&&(tablero[i,j+2])) {
+                alert("Has puesto TRES EN RAYA !!!!!")
+            }
+        }
+    }
 }
