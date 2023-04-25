3db4f9ebe10a878e33a7959ea2452a61da5b803c
javierperezramirez <115183114+javierperezramirez@users.noreply.github.com>
2023-04-25 19:39:04 +0200

    asdasd

--- javascript7.js
+++ javascript7.js
@@ -1,1 +1,23 @@
 'use strict'
+// 'use strict' nos ayuda a no dejar pasar ningun error
+
+// creamos una funcion que pinte el cuadro con un hover
+function hoverino(identificador) {
+    let id="c"+identificador;
+    let cuadro=document.getElementById(id);
+    cuadro.style.backgroundColor='rgb(0, 150, 150)';
+}
+
+// creamos una funcion generica para dibujar el simbolo
+function pintar(identificador) {
+    let id="c"+identificador;
+    let cuadro=document.getElementById(id);
+    cuadro.style.color='black';
+    cuadro.innerHTML="*";
+}
+
+function hoverinoff(identificador) {
+    let id="c"+identificador;
+    let cuadro=document.getElementById(id);
+    cuadro.style.backgroundColor='rgb(0, 218, 218)';
+}
